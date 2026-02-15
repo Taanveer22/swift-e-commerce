54869a17724f1d963fef1ae85a108cc74036b936
Taanveer22 <taanveer469@gmail.com>
2026-02-16 03:26:54 +0600

    all products loaded and showed in the ui via api

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("connected");
 // ========================
 // === load function 01 ===
 // ========================
@@ -7,6 +6,16 @@ const loadCategories = async () => {
   const res = await fetch(url1);
   const data = await res.json();
   displayCategories(data);
+};
+
+// ========================
+// === load function 02 ===
+// ========================
+const loadProducts = async (item) => {
+  const url2 = `https://fakestoreapi.com/products/category/${item}`;
+  const res = await fetch(url2);
+  const data = await res.json();
+  displayProducts(data);
 };
 
 // ========================================================
@@ -18,21 +27,75 @@ const displayCategories = (data) => {
   const categoryBtnContainer = document.getElementById(
     "category-btn-container",
   );
-  console.log(categoryBtnContainer);
-  // categoryBtnContainer.innerHTML = "";
+  // console.log(categoryBtnContainer);
+  categoryBtnContainer.innerHTML = "";
   // step 02
   for (let item of data) {
     // console.log(item);
     // step 03
     const button = document.createElement("button");
+    button.classList.add("btn", "rounded-full");
     button.innerHTML = `
-        <span class="btn btn-ghost rounded-full">
+        <span>
             ${item}
         </span>
   `;
+    // executes on click event listener function
+    button.addEventListener("click", () => {
+      loadProducts(item);
+    });
     // step 04
     categoryBtnContainer.append(button);
   }
+};
+
+// ========================================================
+// === display function 02 ================================
+// ========================================================
+const displayProducts = (items) => {
+  console.log(items);
+  // step 01
+  const productsContainer = document.getElementById("products-container");
+  productsContainer.innerHTML = "";
+  // console.log(productsContainer);
+  // step 02
+  items.forEach((product) => {
+    console.log(product);
+    // step 03
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card bg-base-100 shadow-sm h-80">
+            <figure class="bg-base-300">
+              <img
+                src=${product?.image} class="h-36 py-6"
+              />
+            </figure>
+            <div class="card-body">
+              <div class="flex justify-between">
+                <div class="btn btn-sm rounded-full text-primary">${
+                  product?.category
+                }</div>
+                <div class="flex items-center gap-2">
+                  <i class="fa-solid fa-star text-warning"></i>
+                  <p>${product?.rating.rate}(${product?.rating.count})</p>
+                </div>
+              </div>
+              <p>${product?.title.slice(0, 25)}...</p>
+              <h2 class="card-title">$ ${product?.price}</h2>
+              <div class="card-actions justify-between">
+                <button class="btn btn-sm">
+                  <i class="fa-regular fa-eye"></i><span>Details</span>
+                </button>
+                <button class="btn btn-sm btn-primary">
+                  <i class="fa-solid fa-cart-shopping"></i>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+    productsContainer.append(div);
+  });
 };
 
 // === load function invoke ===
