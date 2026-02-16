6809e446cb38cd37820cfb4bd7e9baa924ea4642
Taanveer22 <taanveer469@gmail.com>
2026-02-17 00:34:31 +0600

    trending now section added via api load

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const loadCategories = async () => {
   const url1 = `https://fakestoreapi.com/products/categories`;
   const res = await fetch(url1);
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
   displayCategories(data);
 };
 
@@ -39,7 +39,19 @@ const loadCardDetails = async (id) => {
   const url3 = `https://fakestoreapi.com/products/${id}`;
   const res = await fetch(url3);
   const data = await res.json();
+  // console.log(data);
   displayCardDetails(data);
+};
+
+// ========================
+// === load function 04 ===
+// ========================
+const loadAllStocks = async () => {
+  const url4 = `https://fakestoreapi.com/products`;
+  const res = await fetch(url4);
+  const data = await res.json();
+  // console.log(data);
+  displayAllStocks(data.slice(0, 3));
 };
 
 // ========================================================
@@ -51,8 +63,9 @@ const displayCategories = (data) => {
   const categoryBtnContainer = document.getElementById(
     "category-btn-container",
   );
+  // stop safely
+  if (!categoryBtnContainer) return;
   // console.log(categoryBtnContainer);
-  categoryBtnContainer.innerHTML = "";
   // step 02
   for (let item of data) {
     // console.log(item);
@@ -60,18 +73,14 @@ const displayCategories = (data) => {
     const button = document.createElement("button");
     button.id = `unique-cat-btn-${item}`;
     button.classList.add("btn", "rounded-full", "shared-cat-btn");
-    button.innerHTML = `
-        <span>
-            ${item}
-        </span>
-  `;
+    button.textContent = ` ${item}`;
     // console.log(button);
     // executes on click event listener function
     button.addEventListener("click", () => {
       loadProducts(item);
     });
     // step 04
-    categoryBtnContainer.append(button);
+    categoryBtnContainer.appendChild(button);
   }
 };
 
@@ -82,6 +91,8 @@ const displayProducts = (items) => {
   // console.log(items);
   // step 01
   const productsContainer = document.getElementById("products-container");
+  // stop safely
+  if (!productsContainer) return;
   productsContainer.innerHTML = "";
   // console.log(productsContainer);
   // step 02
@@ -121,6 +132,7 @@ const displayProducts = (items) => {
             </div>
           </div>
     `;
+    // step 04
     productsContainer.append(div);
   });
 };
@@ -132,6 +144,7 @@ const displayCardDetails = (info) => {
   // console.log(info);
   // step 01
   const modalContainer = document.getElementById("modal-container");
+  if (!modalContainer) return;
   // console.log(modalContainer);
   // step 02
   modalContainer.innerHTML = `
@@ -153,5 +166,56 @@ const displayCardDetails = (info) => {
   document.getElementById("details_modal").showModal();
 };
 
+// ========================================================
+// === display function 04 ================================
+// ========================================================
+const displayAllStocks = (data) => {
+  // console.log(data);
+  // step 01
+  const trendingContainer = document.getElementById("trending-container");
+  if (!trendingContainer) return;
+  // console.log(trendingContainer);
+  // step 02
+  data.forEach((product) => {
+    // step 03
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card bg-base-100 shadow-sm h-80">
+            <figure class="bg-base-300">
+              <img
+                src=${product?.image} class="h-36 py-6"
+              />
+            </figure>
+            <div class="card-body">
+              <div class="flex justify-between">
+                <div class="btn btn-sm rounded-full text-primary">${
+                  product?.category
+                }</div>
+                <div class="flex items-center gap-2">
+                  <i class="fa-solid fa-star text-warning"></i>
+                  <p>${product?.rating.rate}(${product?.rating.count})</p>
+                </div>
+              </div>
+              <p>${product?.title.slice(0, 25)}...</p>
+              <h2 class="card-title">$ ${product?.price}</h2>
+              <div class="card-actions justify-between">
+                <button onclick="loadCardDetails(${product?.id})" class="btn btn-sm">
+                  <i class="fa-regular fa-eye"></i>
+                  <span>Details</span>
+                </button>
+                <button class="btn btn-sm btn-primary">
+                  <i class="fa-solid fa-cart-shopping"></i>
+                  <span>Add</span>
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+    // step 04
+    trendingContainer.append(div);
+  });
+};
+
 // === load function invoke ===
 loadCategories();
+loadAllStocks();
